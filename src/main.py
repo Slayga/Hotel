5112d6dcd6bd86364622008a969ac773e3ca1368
Slayga <gabbe_03_02@hotmail.se>
2022-03-02 21:22:41 +0100

    Datahandling statics && hotelmang. loading file

--- src/main.py
+++ src/main.py
@@ -24,21 +24,73 @@ print(json_content, type(json_content))
 import json
 from typing import Iterable
 
-
+# Todo: Be implemented in HotelManager?
 class DataHandling:
-    def __init__(self):
+    # Should just be extracted from the class and be standalone function instead of method...
+    @staticmethod
+    def create_file(path: str, ext: str = "json"):
+        json.dump(open(str(path + ext), "w")).close()
+        return 1
+
+    # Should just be extracted from the class and be standalone function instead of method...
+    @staticmethod
+    def pack_data(data: Iterable, path: str, mode: str = "w") -> bool:
         ...
 
-    def pack_data(self, data: Iterable, path: str, mode: str = "w") -> bool:
-        ...
-
-    def unpack_data(self) -> Iterable:
-        ...
+    # Should just be extracted from the class and be standalone function instead of method...
+    @staticmethod
+    def unpack_data(path: str) -> dict:
+        # If path is empty or no value
+        if not path:
+            return -1
+        try:
+            with open(path) as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return -1
 
 
 class HotelManager:
-    def __init__(self):
-        ...
+    def __init__(self, path, auto_load: bool = True):
+        # Will automatically load data from given json file...
+        if auto_load:
+            # Tries to unpack data from file
+            self.data = DataHandling().unpack_data()
+            # Incase no file was found at path
+            if self.data == -1:
+                # Create file
+                DataHandling().create_file(path)
+                # Retry unpacking
+                self.data = DataHandling().unpack_data()
+                # If still failing to load raise an exception
+                if self.data == -1:
+                    raise Exception(
+                        "Unexpected error in unpacking file: path:%s".format(
+                            path
+                        )
+                    )
+                else:
+                    self.users = (
+                        self.data["users"] if "users" in self.data else dict()
+                    )
+                    self.rooms = (
+                        self.data["rooms"] if "rooms" in self.data else dict()
+                    )
+                    self.bookings = (
+                        self.data["bookings"]
+                        if "bookings" in self.data
+                        else dict()
+                    )
+                    self.old_books = (
+                        self.data["old_books"]
+                        if "old_books" in self.data
+                        else dict()
+                    )
+        else:
+            self.users = dict()
+            self.rooms = dict()
+            self.bookings = dict()
+            self.old_books = dict()  # Breaking GDPR be like...
 
     def check_in(self):
         ...
