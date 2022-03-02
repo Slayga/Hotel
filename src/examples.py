f1592766742fd7abfa47e646f07a9ba97ecfbf46
Slayga <gabbe_03_02@hotmail.se>
2022-03-02 21:23:03 +0100

    example on empty json file

--- src/examples.py
+++ src/examples.py
@@ -5,20 +5,38 @@ Info:
 Keep track of how to handle certain issues and 
 example solutions//general direction to the solution.
 """
-# ! JSON
-# ? open and read file
-import json  # required...
 
-dict_data = {"name": "bob"}  # data to store (a dict)
-# ? Writing data with context manager // automatically closes file... f.close(), '(__exit__)'
-with open("src/example.json", "w") as f:
-    json.dump(dict_data, f)  # dumps the data into the file...
-    #! note the mode in open("...", "w") -> "w" will overwrite the current content of file
 
-# ? Reading data with context manager...
-with open("src/bobdump.json") as f:
-    json_content = json.load(f)  # json.loads(<file>) will return the dict
-    # No need to pass mode in open() as default is "r" (read)
+def test_basic_json():
+    # ! JSON
+    # ? open and read file
+    import json  # required...
 
-# Fact check the results from the loading...
-print(json_content, type(json_content))
+    dict_data = {"name": "bob"}  # data to store (a dict)
+    # ? Writing data with context manager // automatically closes file... f.close(), '(__exit__)'
+    with open("src/example.json", "w") as f:
+        json.dump(dict_data, f)  # dumps the data into the file...
+        #! note the mode in open("...", "w") -> "w" will overwrite the current content of file
+
+    # ? Reading data with context manager...
+    with open("src/bobdump.json") as f:
+        json_content = json.load(f)  # json.loads(<file>) will return the dict
+        # No need to pass mode in open() as default is "r" (read)
+
+    # Fact check the results from the loading...
+    print(json_content, type(json_content))
+
+
+def test_empty_json():
+    import json
+
+    with open("src/example.json", "w") as f:
+        json.dump({}, f)
+    with open("src/example.json") as f:
+        empty_json = json.load(f)
+    print(empty_json)
+
+
+if __name__ == "__main__":
+    # test_basic_json()
+    test_empty_json()
