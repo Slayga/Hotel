4887b82499d3022f93275d318798624605139207
Slayga <63261321+Slayga@users.noreply.github.com>
2022-03-07 09:53:55 +0100

    data

--- src/main.py
+++ src/main.py
@@ -9,76 +9,67 @@ Info: Main running file for this application
 
 import json
 
-# Todo: Be implemented in HotelManager?
+# Todo: Be implemented in HotelManager? Or... Extarct methods to standalone functions
 class DataHandling:
+    def __init__(self, path, fallBackName: str = "/hotel", ext: str = ".json"):
+        # Check if path is valid and if the json file was given
+        # else add fallBackName and .json to path.
+        if path is None:
+            self.path = fallBackName + ext
+        else:
+            self.path = path if path.endswith(ext) else self.path + fallBackName + ext
+
     # Should just be extracted from the class and be standalone function instead of method...
-    @staticmethod
-    def create_file(path: str, ext: str = ".json"):
-        path = path if path.endswith(ext) else path + ext
+    def create_file(self):
         # Creates a empty file with empty dict
-        with open(str(path + ext), "w") as f:
+        with open(str(self.path), "w") as f:
             json.dump({}, f)
         return 1
 
     # Should just be extracted from the class and be standalone function instead of method...
-    @staticmethod
-    def pack_data(data: dict, path: str, mode: str = "w") -> bool:
-        ...
+    def pack_data(self, data: dict, mode: str = "w") -> bool:
+        with open(self.path, mode) as f:
+            json.dump(data, f)
 
     # Should just be extracted from the class and be standalone function instead of method...
-    @staticmethod
-    def unpack_data(path: str) -> dict:
+    def unpack_data(self) -> dict:
         # If path is empty or no value
-        if not path:
-            return -1
         try:
-            with open(path) as f:
+            with open(self.path) as f:
                 return json.load(f)
         except FileNotFoundError:
             return -1
 
 
 class HotelManager:
-    def __init__(self, path, auto_load: bool = True):
+    def __init__(self, path):
         self.path = path
-        # Will automatically load data from given json file...
-        if auto_load:
-            # Tries to unpack data from file
+        # Tries to unpack data from file
+        self.data = DataHandling().unpack_data(self.path)
+        # Incase no file was found at path
+        if self.data == -1:
+            # Create file
+            DataHandling().create_file(path)
+            # Retry unpacking
             self.data = DataHandling().unpack_data(self.path)
-            # Incase no file was found at path
+            # If still failing to load raise an exception
             if self.data == -1:
-                # Create file
-                DataHandling().create_file(path)
-                # Retry unpacking
-                self.data = DataHandling().unpack_data(self.path)
-                # If still failing to load raise an exception
-                if self.data == -1:
-                    raise Exception(
-                        "Unexpected error in unpacking file: path:'{}'".format(
-                            path
-                        )
-                    )
-                else:
-                    # Basically if the json file has saved data already
-                    # it assigns the values (dicts) to the attribute if
-                    # the key is in the data... see below:
-                    # data_a = data["a"] if "a" in data else dict()
-                    self.users = (
-                        self.data["users"] if "users" in self.data else dict()
-                    )
-                    self.rooms = (
-                        self.data["rooms"] if "rooms" in self.data else dict()
-                    )
-                    self.bookings = (
-                        self.data["bookings"]
-                        if "bookings" in self.data
-                        else dict()
-                    )
-                    self.old_books = (
-                        self.data["old_books"]
-                        if "old_books" in self.data
-                        else dict()
-                    )
+                raise Exception(
+                    "Unexpected error in unpacking file: path:'{}'".format(path)
+                )
+            else:
+                # Basically if the json file has saved data already
+                # it assigns the values (dicts) to the attribute if
+                # the key is in the data... see below:
+                # data_a = data["a"] if "a" in data else dict()
+                self.users = self.data["users"] if "users" in self.data else dict()
+                self.rooms = self.data["rooms"] if "rooms" in self.data else dict()
+                self.bookings = (
+                    self.data["bookings"] if "bookings" in self.data else dict()
+                )
+                self.old_books = (
+                    self.data["old_books"] if "old_books" in self.data else dict()
+                )
         # Should just load data automatically...remove the implementation of
         # auto_load and always load file on init. As it creates file when no file...
         else:
