83afe529a1a49cfee2a0ac60cee4585cca7601d0
Slayga <63261321+Slayga@users.noreply.github.com>
2022-03-11 09:16:20 +0100

    centralized testing (/test)

--- test/testing.py
+++ test/testing.py
@@ -6,6 +6,10 @@ Keep track of how to handle certain issues and
 example solutions//general direction to the solution.
 """
 import json
+import imgui
+import os
+
+os.makedirs("test", exist_ok=True)
 
 
 def test_basic_json():
@@ -15,10 +19,10 @@ def test_basic_json():
 
     dict_data = {"name": "bob"}  # data to store (a dict)
     # ? Writing data with context manager // automatically closes file... f.close(), '(__exit__)'
+    # Always write keys as strings and avoid int:
     with open("src/json/example.json", "w") as f:
         json.dump(dict_data, f)  # dumps the data into the file...
-        #! note the mode in open("...", "w") -> "w" will overwrite the current content of file
-
+        # ! note the mode in open("...", "w") -> "w" will overwrite the current content of file
     # ? Reading data with context manager...
     with open("src/json/bobdump.json") as f:
         json_content = json.load(f)  # json.loads(<file>) will return the dict
@@ -31,11 +35,18 @@ def test_basic_json():
 def test_empty_json():
     import json
 
+    # Write an empty dict to not crash when reading json...
     with open("src/json/example.json", "w") as f:
         json.dump({}, f)
     with open("src/json/example.json") as f:
         empty_json = json.load(f)
     print(empty_json)
+
+
+def test_writing_json():
+    # {str(): int()} is acceptable
+    # {int(): int()} will be converted by json.dump(...) to {str(): int()}
+    ...
 
 
 def test_pretty_print(content, indent: int = 0):
@@ -52,8 +63,13 @@ def test_pretty_print(content, indent: int = 0):
 
 
 if __name__ == "__main__":
+    # 1
     # test_basic_json()
+    # 2
     # test_empty_json()
-    with open("src/json/bobdump.json") as f:
-        json_content = json.load(f)
-    test_pretty_print(json_content)
+    # 3
+    # with open("src/json/bobdump.json") as f:
+    #     json_content = json.load(f)
+    # test_pretty_print(json_content)
+    # 4
+    ...
