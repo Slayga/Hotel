7f524dc4282d25d322f2a6b4d5513bd9e2d4f3ef
Slayga <gabbe_03_02@hotmail.se>
2022-03-12 17:44:36 +0100

    test

--- test/testing.py
+++ test/testing.py
@@ -4,19 +4,28 @@ Date: 02-03-2022
 Info:
 Keep track of how to handle certain issues and 
 example solutions//general direction to the solution.
+
+Quick TL:DR:
+        1. Use context manager to write and read json.
+        2. Always have the keys in the dict being of type string.
+        3. Always make sure to have at least an empty dict in the json file.
+            (If you read an empty json file the program will crash...)
+
 """
 import json
 import imgui
 import os
 
-# Create folder "json" in the current working directory
+# Note: That path is just to get the exact path to the working directory.
 path = os.path.dirname(__file__)
+# Create folder "json" in the current working directory.
 if not os.path.exists(path + "\\json"):
     os.makedirs(path + "\\json", exist_ok=True)
 
 
 def test_basic_json():
-    """Basics for writing and reading json files.
+    """
+    Basics for writing and reading json files.
 
     Returns:
         None: None
@@ -27,15 +36,15 @@ def test_basic_json():
     dict_data = {"name": "bob", "misc": {"1": [1, 2, 3, 4]}}
 
     # ? Writing data with context manager // will automatically close file... f.close(), '(__exit__)'
-    with open(path + "/json/example.json", "w") as f:
+    with open(path + "/json/test_example.json", "w") as f:
         # Always have keys as strings ({"1": 1}) and avoid int ({1: 1}):
         json.dump(dict_data, f)  # dumps the data into the file...
-        # ! note the mode in open("...", "w") -> "w" will overwrite the file not append
+        # ! note the mode in open("...", "w") -> "w" will overwrite the file. Not append to it
 
     # ? Reading data with context manager...
-    with open(path + "/json/example.json") as f:
-        json_content = json.load(f)  # json.loads(<file>) will return the dict
-        # No need to pass mode in open() as default is "r" (read)
+    # No need to pass mode in open() as default is "r" (read)
+    with open(path + "/json/test_example.json") as f:
+        json_content = json.load(f)  # json.load(<file>) will return the dict
 
     # Fact check the results from the read (json.load(f))...
     print(json_content, type(json_content))
@@ -50,39 +59,28 @@ def test_empty_json():
 
     with open(path + "/json/test_empty.json") as f:
         empty_json = json.load(f)
+
     print(empty_json)
 
 
 def test_writing_json():
-    # Below is acceptable way to stor & write json
+    # Below is acceptable way to store & write json
     dict_str_key = {str(): int()}
     # While this will be converted by json.dump(...) to dict_str_key
     dict_int_key = {int(): int()}
-    ...
 
+    with open(path + "/json/test_write_str_key.json", "w") as f:
+        json.dump(dict_str_key, f)
 
-def test_pretty_print(content, indent: int = 0):
-    # How to read and pretty-print the dicts.
-    # This is just to visually see the what is assigned to what...
-    if isinstance(content, dict):
-        for k, v in content.items():
-            print(" " * indent, k)
-            if isinstance(v, dict):
-                test_pretty_print(v, indent + 2)  # Recursive
-            else:
-                print(" " * (indent + 1), v)
-    else:
-        print(" " * (indent + 1), content)
+    with open(path + "/json/test_write_int_key.json", "w") as f:
+        json.dump(dict_int_key, f)
 
 
 if __name__ == "__main__":
     # 1
     test_basic_json()
     # 2
-    test_empty_json()
+    test_writing_json()
     # 3
-    with open("test/json/bobdump.json") as f:
-        json_content = json.load(f)
-    test_pretty_print(json_content)
-    # 4
+    test_empty_json()
     ...
