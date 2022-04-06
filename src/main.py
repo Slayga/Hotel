c5f4a1f05dc03461d904ec76e60f423712d31edb
Slayga <gabbe_03_02@hotmail.se>
2022-04-06 18:24:44 +0200

    typing ++ commenting

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 """
 Name: Gabriel Engberg, Viggo Rubin
 Date: 28-02-2022
-Info: Main running file for this application
+Info: Main running file for hotel application. 
+This is in theory meant to be used by personal at a given hotel, 
+hence the management of seeing SSN easily.
 """
 
 # Pickle or Json dump...
@@ -10,27 +12,43 @@ Info: Main running file for this application
 import json
 import os
 
-# Todo: Be implemented in HotelManager? Or... Extract methods to standalone functions
+
 class DataHandling:
-    def __init__(self, filename: str):
+    """
+    Class for handling data from json files
+    """
+
+    def __init__(self, filename: str = "hotel.json"):
+        """
+        Constructor for DataHandling
+
+        Args:
+            filename (str, optional): Name of the file to be used. Defaults to hotel.json.
+        """
+        # For a particular reason, this is needed to call the filename setter
         self._filename = self.filename = filename
         self._folder = "json"
+        # Gets absolute path to working directory...
         self._path = os.path.dirname(__file__) + "/" + self.folder
+        # Absolute path to file (file included)
         self.full_path = self.path + "/" + self._filename
         # Create self.folder in the current working directory.
         if not os.path.exists(self._path):
-            #
+            # Make a folder called json in directory if not existing
             os.makedirs(self._path, exist_ok=True)
 
-        if not self.__file_exists(self.full_path):
+        # Creates the .json file if it doesn't exist.
+        if not os.path.exists(self.full_path):
             self.__create_file(self.full_path)
 
     @property
     def path(self) -> str:
+        """Property for path"""
         return self._path
 
     @property
     def folder(self) -> str:
+        """Property for folder"""
         return self._folder
 
     @property
@@ -39,18 +57,21 @@ class DataHandling:
         return self._filename
 
     @path.setter
-    def setter(self, value: str):
-        raise ValueError("Path cant be changed")
+    def setter(self, _: str):
+        """Setter for path, forcing immutable attr...kinda"""
+        raise ValueError("Path attr. cant be changed")
 
     @folder.setter
-    def folder(self, value: str):
-        raise ValueError("Folder cant be changed")
+    def folder(self, _: str):
+        """Setter for folder, forcing immutable attr...kinda"""
+        raise ValueError("Folder attr. cant be changed")
 
     @filename.setter
     def filename(self, value: str):
         """Setter for filename"""
-        # Instead of raising an exception on 'empty filename' a fallback exists.
+        # Instead of raising an exception on no 'filename' a fallback exists.
         self.__fallback = "hotel.json"
+        # Evaluate whetever the custom value(if given is a valid file)
         if value:
             if value.endswith(".json"):
                 self._filename = value
@@ -59,23 +80,39 @@ class DataHandling:
         else:
             self._filename = self.__fallback
 
-    def __file_exists(self, path: str) -> bool:
-        return os.path.exists(path)
-
     def __create_file(self, path: str):
+        """
+        Creates an 'empty' json file
+
+        Args:
+            path (str): Given path to file
+        """
+        # Loads an empty dict into the json file, or it will crash on read.
+        # See testing.py in 'test' folder for more details.
         with open(str(path), "w") as f:
             json.dump({}, f)
 
-    def pack_data(self, data: dict, mode: str = "w") -> bool:
+    def pack_data(self, data: dict, mode: str = "w"):
+        """
+        Writes data to a json file
+
+        Args:
+            data (dict): Data structure of data to be stored,
+                        #! NOTE that all keys must be of type str
+            mode (str, optional): Mode the file will be open in. Defaults to "w".
+        """
         with open(self.full_path, mode) as f:
             json.dump(data, f)
 
     def unpack_data(self) -> dict:
-        try:
-            with open(self.full_path) as f:
-                return json.load(f)
-        except FileNotFoundError:
-            raise FileNotFoundError("Unresolved file error:", self.filename)
+        """
+        Opens json file and returns the data structure as a dictionary
+
+        Returns:
+            dict: data stored in json file as a dictionary.
+        """
+        with open(self.full_path) as f:
+            return json.load(f)
 
 
 class HotelManager:
@@ -84,9 +121,9 @@ class HotelManager:
     Used to manipulate data from given file that class DataHandling returns
     when unpacking.
 
-    HotelManager uses methods for checking in, checking out,
+    HotelManager uses methods for: checking in, checking out,
     adding bookings, removing bookings, editing bookings, adding rooms,
-    removing rooms, editing rooms, and printing data.
+    removing rooms, editing rooms, and printing raw data.
     """
 
     def __init__(self, filename: str = ""):
@@ -119,17 +156,15 @@ class HotelManager:
     def edit_booking(self):
         ...
 
-    def filter_rooms(self, filter_:dict = None) -> list:
-        # key: What key to check for each room, value is the value of the key
+    def filter_rooms(self, filter_: dict = None) -> list[dict] | dict:
         # Check if filter_ is provided (underscore is to avoid naming conflict)
-        filter_ = False if filter_ is None else filter_
         if filter_:
+            # Example of filter check filter_ = {"state": "vacant"}
             for room in self.rooms:
                 ...
         else:
             # If no filter was given, return self.rooms (all rooms)
             return self.rooms
-        
 
     def add_room(self):
         ...
